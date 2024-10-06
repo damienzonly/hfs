adb1ca393d116990bd3e6fe1288420ee4ab0b085
Massimo Melina <a@rejetto.com>
2024-10-06 17:34:08 +0200

    webdav

--- src/cross-const.ts
+++ src/cross-const.ts
@@ -15,6 +15,7 @@ export const PREVIOUS_TAG = 'previous'
 export const ALLOW_SESSION_IP_CHANGE = 'allow_session_ip_change'
 
 export const HTTP_OK = 200
+export const HTTP_CREATED = 201
 export const HTTP_NO_CONTENT = 204
 export const HTTP_PARTIAL_CONTENT = 206
 export const HTTP_MOVED_PERMANENTLY = 301
